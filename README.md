c33da6388503c6947846fe98ae2628b11e18dde9
magicdirac <magicdirac@users.noreply.github.com>
2016-06-30 04:01:04 -0400

    initial init. avy-flycheck-goto-error.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # avy-flycheck
+`avy-flycheck` is a GNU Emacs package for syntax errors navigation. It depends on `flycheck` to provide syntax error checking and `avy` for easy navigation.
+
 Jump to and fix syntax errors using `flycheck` with `avy` interface
+
+## Configuration overview
+
+### add path of this package to your `load-path`
+```elisp
+(add-to-list 'load-path
+	     "path/to/the/directory/of/avy-flycheck")
+```
+then simply `require` the package
+```elisp
+(require 'avy-flycheck)
+```
+
+### bind to a key for your convenience.
+>you can choose your own binding.
+```elisp
+(global-set-key (kbd "C-c ;") #'avy-flycheck-goto-error)
+```
+
+### A few customizations are provided.
+>Set Method for displaying avy overlays.
+```elisp
+(setq avy-flycheck-style 'pre)
+```
+
+## Contributing
+
+### Copyright Assignment
+
+`avy-flycheck` is subject to the same [copyright assignment](http://www.gnu.org/prep/maintain/html_node/Copyright-Papers.html) policy as Emacs itself, org-mode, CEDET and other packages in [GNU ELPA](http://elpa.gnu.org/packages/). Any [legally significant](http://www.gnu.org/prep/maintain/html_node/Legally-Significant.html#Legally-Significant) contributions can only be accepted after the author has completed their paperwork. Please see [the request form](http://git.savannah.gnu.org/cgit/gnulib.git/tree/doc/Copyright/request-assign.future) if you want to proceed.
+
+The copyright assignment isn't a big deal, it just says that the copyright for your submitted changes to Emacs belongs to the FSF. This assignment works for all projects related to Emacs. To obtain it, you need to send one email, then send one letter (if you live in the US, it's digital), and wait for some time (in my case, I had to wait for one month).
+
+### Style
+
+Use your own judgment for the commit messages, I recommend a verbose style using `magit-commit-add-log`.
---
+++ avy-flycheck.el
@@ -0,0 +1,144 @@
+;;; avy-flycheck.el --- Jump to and fix syntax errors using `flycheck' with `avy' interface -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Xu Ma
+
+;; Author: Xu Ma <magicdirac@gmail.com>
+;; URL: https://github.com/magicdirac/flycheck
+;; Package-Version: 0.0.1
+;; Version: 0.0.1
+;; Package-Requires: ((avy "0.4.0")) ((flycheck))
+;; Keywords: avy flycheck
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 1. Load the package
+;; ===================
+
+;; ,----
+;; | (add-to-list 'load-path "/path/to/avy-flycheck.el")
+;; | (require 'avy-flycheck)
+;; `----
+
+;; 2. Give the command a keybinding
+;; ================================
+
+;; (global-set-key (kbd "C-c ;") #'avy-flycheck-goto-error)
+
+;; 3 Acknowledgment
+;; ================
+
+;; This package is based on [abo-abo(Oleh Krehel)]'s awesome [avy] package.
+
+;; [abo-abo(Oleh Krehel)] https://github.com/abo-abo/
+
+;; [avy] https://github.com/abo-abo/avy
+
+;;; Code:
+
+
+(require 'avy)
+(require 'flycheck)
+
+(defgroup avy-flycheck nil
+  "Jump to and fix syntax errors `flycheck' with `avy' interface"
+  :group 'flycheck)
+
+(defcustom avy-flycheck-style 'pre
+  "Method for displaying avy overlays.
+Defaults to pre."
+  :group 'avy-flycheck
+  :type '(choice
+          (const :tag "Pre" pre)
+          (const :tag "At" at)
+          (const :tag "At Full" at-full)
+          (const :tag "Post" post)
+          (const :tag "De Bruijn" de-bruijn)))
+
+(defun avy--flycheck--cands (&optional arg beg end)
+  (let (candidates)
+    (avy-dowindows arg
+      (let ((top (or beg (window-start)))
+            (bot (or end (window-end))))
+        (save-excursion
+          (save-restriction
+            (narrow-to-region top bot)
+            (overlay-recenter (point-max))
+            ;; TODO: check how to deal with multiple times overlayed region.
+            (let ((overlay-list (overlays-in (point-min) (point-max))))
+              (setq candidates
+                    (append
+                     (mapcar
+                      (lambda (element)
+                        (cons
+                         (if (eq avy-flycheck-style 'post)
+                             (overlay-end element)
+                           (overlay-start element))
+                         (selected-window)))
+                      (remove-if
+                       (lambda (element)
+                         (let ((pos (overlay-start element)))
+                           (not (and (get-char-property pos 'flycheck-error)
+                                     ;; Check if this error is interesting
+                                     (flycheck-error-level-interesting-at-pos-p pos)))))
+                      overlay-list))
+                    candidates)))))))
+    candidates))
+
+(defun avy--flycheck (&optional arg beg end)
+"Select a flycheck syntax error.
+The window scope is determined by `avy-all-windows' (ARG negates it).
+Narrow the scope to BEG END."
+  (let ((avy-action #'identity)
+        (avy-style 'post)
+        (candidates (avy--flycheck--cands arg beg end)))
+    (if candidates
+        (progn
+          (if (= 1 (length candidates))
+              (message "There is only one Syntax error and jump to it"))
+          (avy--process
+           candidates
+           (avy--style-fn avy-style))))
+    (progn
+      (message "There is no Syntax error found.")
+      nil)))
+
+;;;###autoload
+(defun avy-flycheck-goto-error (&optional arg)
+  "Jump to a flycheck syntax error.
+The window scope is determined by `avy-all-windows' (ARG negates it)."
+  (interactive (list current-prefix-arg))
+  (avy-with avy-flycheck-jump-word
+    (let* ((r (avy--flycheck (eq arg 4))))
+      (unless (or (not r) (eq r t))
+        (avy-action-goto r)))))
+
+;; ;;;###autoload
+;; (defun avy-flycheck-setup ()
+;;   "Set up default keybindings."
+;;   (interactive)
+;;   (eval-after-load "flycheck"
+;;     ;; TODO: need to fix why I can not add new command to `flycheck-mode-map'
+;;     '(define-key flycheck-mode-map (kbd "C-c ! g") #'avy-flycheck-goto-error)
+;;     ))
+
+(provide 'avy-flycheck)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; avy-flycheck.el ends here
